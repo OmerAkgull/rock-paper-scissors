50221f6e251123f92b56e1a1d5fff5c94e808420
OmerAkgull <143122832+OmerAkgull@users.noreply.github.com>
2023-11-29 17:07:47 +0300

    Create function to play round

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 function getComputerChoice() {
-    let options = ["rock", "paper", "scissors"]
-    let choice = options[Math.floor(Math.random()*options.length)];
+  let options = ["rock", "paper", "scissors"];
+  let choice = options[Math.floor(Math.random() * options.length)];
 
-    return choice
+  return choice;
 }
 
+const computerSelection = getComputerChoice();
+const playerSelection = "rock";
+const playerSelectionFixed = playerSelection.toLowerCase();
+
+function playRound(playerSelection, computerSelection) {
+  if ((computerSelection === "paper") & (playerSelection === "rock")) {
+    return "You lose! Paper beats rock.";
+  } else if (
+    (computerSelection === "scissors") &
+    (playerSelection === "rock")
+  ) {
+    return "You win! Rock beats scissors.";
+  } else {
+    playRound(playerSelection, computerSelection);
+  }
+}
+
+console.log("Player:", playerSelection, "Computer:", computerSelection);
+playRound(playerSelectionFixed, computerSelection);
