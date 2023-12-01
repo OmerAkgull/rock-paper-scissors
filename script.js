71529970d02e3fd9f995847305c94c3f88758eee
OmerAkgull <143122832+OmerAkgull@users.noreply.github.com>
2023-12-01 13:02:24 +0300

    Add player prompt and add other results

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ const playerSelection = prompt("Rock, paper, or scissors?");
 const playerSelectionFixed = playerSelection.toLowerCase();
 
 function playRound(playerSelection, computerSelection) {
+  console.log("Player:", playerSelection, "Computer:", computerSelection);
+
   if ((computerSelection === "paper") & (playerSelection === "rock")) {
     return "You lose! Paper beats rock.";
   } else if (
@@ -17,10 +19,26 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === "rock")
   ) {
     return "You win! Rock beats scissors.";
+  } else if (
+    (computerSelection === "scissors") &
+    (playerSelection === "paper")
+  ) {
+    return "You lose! Scissors beats paper.";
+  } else if ((computerSelection === "rock") & (playerSelection === "paper")) {
+    return "You win! Paper beats rock.";
+  } else if (
+    (computerSelection === "rock") &
+    (playerSelection === "scissors")
+  ) {
+    return "You lose! Rock beats scissors.";
+  } else if (
+    (computerSelection === "paper") &
+    (playerSelection === "scissors")
+  ) {
+    return "You win! Scissors beats paper.";
   } else {
     playRound(playerSelectionFixed, getComputerChoice());
   }
 }
 
-console.log("Player:", playerSelection, "Computer:", computerSelection);
 playRound(playerSelectionFixed, computerSelection);
